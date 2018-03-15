b091045af4edb5130627ebd581d0c6313c86265f
Ara Ayvazyan <Aivazyan@microsoft.com>
2018-03-14 20:04:41 -0700

    Basic message support

--- tools/protoc-plugin/main.cpp
+++ tools/protoc-plugin/main.cpp
@@ -1,13 +1,15 @@
-#pragma warning(disable:4127)
+#pragma warning(disable:4127 4146 4800)
 
 #include <google/protobuf/compiler/plugin.h>
 #include <google/protobuf/compiler/code_generator.h>
 #include <google/protobuf/descriptor.h>
+#include <google/protobuf/descriptor.pb.h>
 #include <google/protobuf/io/zero_copy_stream.h>
 #include <google/protobuf/io/printer.h>
 
-#include <google/protobuf/stubs/scoped_ptr.h>
 #include <google/protobuf/stubs/strutil.h>
+
+#include <memory>
 
 
 namespace bond
@@ -16,50 +18,180 @@ namespace detail
 {
 namespace proto
 {
-    class IdlGenerator : public google::protobuf::compiler::CodeGenerator
+    class FieldGenerator
+    {
+    public:
+        explicit FieldGenerator(google::protobuf::io::Printer& printer)
+            : _printer{ printer }
+        {}
+
+        void Generate(const google::protobuf::FieldDescriptor& field)
+        {
+            if (auto packing = GetFieldPacking(field))
+            {
+                _printer.Print("[ProtoPack(\"$packing$\")]\n",
+                    "packing", packing);
+            }
+
+            if (auto encoding = GetFieldEncoding(field))
+            {
+                _printer.Print("[ProtoEncode(\"$encoding$\")]\n",
+                    "encoding", encoding);
+            }
+
+            _printer.Print("$required$$id$: $type$ $name$;\n",
+                "required", field.label() == google::protobuf::FieldDescriptor::LABEL_REQUIRED ? "required" : "",
+                "id", std::to_string(field.number()),
+                "type", GetFieldTypeName(field),
+                "name", field.name());
+        }
+
+    private:
+        static const char* GetFieldPacking(const google::protobuf::FieldDescriptor& field)
+        {
+            return field.is_packable() && !field.is_packed() ? "False" : nullptr;
+        }
+
+        static const char* GetFieldEncoding(const google::protobuf::FieldDescriptor& field)
+        {
+            switch (field.type())
+            {
+            case google::protobuf::FieldDescriptor::TYPE_FIXED64:
+            case google::protobuf::FieldDescriptor::TYPE_FIXED32:
+            case google::protobuf::FieldDescriptor::TYPE_SFIXED32:
+            case google::protobuf::FieldDescriptor::TYPE_SFIXED64:
+                return "Fixed";
+
+            case google::protobuf::FieldDescriptor::TYPE_SINT32:
+            case google::protobuf::FieldDescriptor::TYPE_SINT64:
+                return "ZigZag";
+            }
+
+            return nullptr;
+        }
+
+        static google::protobuf::string GetFieldTypeName(const google::protobuf::FieldDescriptor& field)
+        {
+            switch (field.cpp_type())
+            {
+            case google::protobuf::FieldDescriptor::CPPTYPE_INT32:
+                return "int32";
+
+            case google::protobuf::FieldDescriptor::CPPTYPE_INT64:
+                return "int64";
+
+            case google::protobuf::FieldDescriptor::CPPTYPE_UINT32:
+                return "uint32";
+
+            case google::protobuf::FieldDescriptor::CPPTYPE_UINT64:
+                return "uint64";
+
+            case google::protobuf::FieldDescriptor::CPPTYPE_DOUBLE:
+                return "double";
+
+            case google::protobuf::FieldDescriptor::CPPTYPE_FLOAT:
+                return "float";
+
+            case google::protobuf::FieldDescriptor::CPPTYPE_BOOL:
+                return "bool";
+
+            case google::protobuf::FieldDescriptor::CPPTYPE_ENUM:
+                return field.enum_type()->name();
+
+            case google::protobuf::FieldDescriptor::CPPTYPE_STRING:
+                return field.type() == google::protobuf::FieldDescriptor::TYPE_BYTES ? "blob" : "string";
+
+            case google::protobuf::FieldDescriptor::CPPTYPE_MESSAGE:
+                return field.options().lazy()
+                    ? ("bonded<" + field.message_type()->name() + ">")
+                    : field.message_type()->name();
+            }
+
+            assert(false);
+            return nullptr;
+        }
+
+        google::protobuf::io::Printer& _printer;
+    };
+
+
+    class StructGenerator
+    {
+    public:
+        explicit StructGenerator(google::protobuf::io::Printer& printer)
+            : _printer{ printer }
+        {}
+
+        void Generate(const google::protobuf::Descriptor& msg)
+        {
+            _printer.Print("\nstruct $name$\n{\n",
+                "name", msg.name());
+            _printer.Indent();
+
+            FieldGenerator field_generator{ _printer };
+
+            for (int i = 0; i < msg.field_count(); ++i)
+            {
+                field_generator.Generate(*msg.field(i));
+            }
+
+            _printer.Outdent();
+            _printer.Print("}\n");
+        }
+
+    private:
+        google::protobuf::io::Printer& _printer;
+    };
+
+
+    class FileGenerator
+    {
+    public:
+        void Generate(const google::protobuf::FileDescriptor& file, google::protobuf::compiler::GeneratorContext& context)
+        {
+            auto basename = google::protobuf::StripSuffixString(file.name(), ".proto");
+
+            std::unique_ptr<google::protobuf::io::ZeroCopyOutputStream> output{ context.Open(basename + ".bond") };
+            google::protobuf::io::Printer printer{ output.get(), '$' };
+
+            printer.Print("namespace $namespace$\n",
+                "namespace", file.package());
+
+            StructGenerator struct_generator{ printer };
+
+            for (int i = 0; i < file.message_type_count(); ++i)
+            {
+                struct_generator.Generate(*file.message_type(i));
+            }
+        }
+    };
+
+
+    class Generator : public google::protobuf::compiler::CodeGenerator
     {
     public:
         bool Generate(const google::protobuf::FileDescriptor* file,
             const google::protobuf::string& /*parameter*/,
             google::protobuf::compiler::GeneratorContext* generator_context,
-            google::protobuf::string* /*error*/) const override
+            google::protobuf::string* error) const override
         {
-            auto basename = google::protobuf::StripSuffixString(file->name(), ".proto");
-
-            google::protobuf::scoped_ptr<google::protobuf::io::ZeroCopyOutputStream> output{
-                generator_context->Open(basename + ".bond") };
-
-            google::protobuf::io::Printer printer{ output.get(), '$' };
-
-            printer.Print("namespace $namespace$\n",
-                "namespace", file->package());
-
-            for (int i = 0; i < file->message_type_count(); ++i)
+            try
             {
-                auto msg = file->message_type(i);
-
-                printer.Print("\nstruct $name$\n{\n",
-                    "name", msg->name());
-                printer.Indent();
-
-                for (int j = 0; j < msg->field_count(); ++j)
-                {
-                    auto field = msg->field(j);
-
-                    printer.Print("$id$: $type$ $name$;\n",
-                        "id", std::to_string(field->number()),
-                        "type", field->cpp_type_name(),
-                        "name", field->name());
-                }
-
-                printer.Outdent();
-                printer.Print("}\n");
+                FileGenerator file_generator;
+                file_generator.Generate(*file, *generator_context);
+                return true;
             }
-
-            return true;
+            catch (const std::exception& e)
+            {
+                *error = e.what();
+                return false;
+            }
+            catch (...)
+            {
+                *error = "Unknown error";
+                return false;
+            }
         }
-
-    private:
     };
 
 }}} // namespace bond::detail::proto
@@ -67,6 +199,6 @@ namespace proto
 
 int main(int argc, char* argv[])
 {
-    bond::detail::proto::IdlGenerator generator;
+    bond::detail::proto::Generator generator;
     return google::protobuf::compiler::PluginMain(argc, argv, &generator);
 }
